1433cce5a34100ec1ca195c6a8d0e44bec515cea
Jesus Rodriguez Barrera <jesus@macbook-pro-de-jesus.local>
2016-03-13 16:18:10 -0600

    commit

--- BusquedaOpenLibrary/ViewController.swift
+++ BusquedaOpenLibrary/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var isbnTexto: UITextField!
+    @IBOutlet weak var salidaTexto: UITextView!
+    
+    func sincrono(isbn:String){
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn)"
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        if datos == nil{
+            salidaTexto.text = "Error con la conexion a internet."
+        }else{
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            salidaTexto.text = texto! as String
+        }
+    }
+    @IBAction func backGroundTap(sender: AnyObject) {
+        isbnTexto.resignFirstResponder() //Desaparecer el teclado
+    }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder() //Desaparecer el teclado
+        sincrono(isbnTexto.text!)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        isbnTexto.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
